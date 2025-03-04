f2c8229448509cf52c27179970f9f1691fcc46ea
Luciano <62312023+lucianock@users.noreply.github.com>
2025-03-04 13:24:37 -0300

    user menu down

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -24,9 +24,9 @@
 
 
 
-            {{-- <flux:spacer />
+           <flux:spacer />
 
-            <flux:navlist variant="outline">
+             {{-- <flux:navlist variant="outline">
                 <flux:navlist.item icon="folder-git-2" href="https://github.com/laravel/livewire-starter-kit" target="_blank">
                 {{ __('Repository') }}
                 </flux:navlist.item>
